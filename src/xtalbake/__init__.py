d6d3fbf311e1b5459ee39be9f1658c4c3ce827ef
Peter Barrow <peter.barrow.93@gmail.com>
2025-12-04 14:38:17 +0100

    chore: __all__ for __init__.py

--- src/xtalbake/__init__.py
+++ src/xtalbake/__init__.py
@@ -1,3 +1,10 @@
 from .device_discovery import discover, create_controller
 from .mtd1020t import MTD1020T
 from .covesion_oc3 import OC3
+
+__all__ = [
+    'create_controller',
+    'discover',
+    'MTD1020T',
+    'OC3',
+]
